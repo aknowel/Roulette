41cd455126d58884537141f39b256eed078482df
Paweł Nowak <paweln.nowak@student.uj.edu.pl>
2021-03-03 22:00:21 +0100

    Add MM SL TP

--- src/main.py
+++ src/main.py
@@ -16,26 +16,32 @@ colorama.init(autoreset=True)
 handwritten_filepaths = ['../data/handwritten/list{}.txt'.format(i) for i in range(1, 23)]
 generated_filepaths = ['../data/generated/gen{}.txt'.format(i) for i in range(1, 23)]
 
-K = 100
+KFULL = 1000
 
 config = Loader.load_config('config.json')
 config = RouletteAnalyzer.generate_from_config(config)
 
-vals = list()
-for _ in range(10):
-    l = Loader.random_sample(handwritten_filepaths, 100)
-    vals += l
 
-x_vals = [float(i) for i in Simulator.simulate_bubbles_and_progression(vals, config, K)]
+vals = list()
+vals.append(KFULL)
+for _ in range(1000):
+    l = Loader.random_sample(handwritten_filepaths, 150)
+    K = 0.1 * KFULL
+    KFULL -= K
+    K = Simulator.simulate_bubbles_and_progression(l, config, K)[-1]
+    KFULL += K
+    vals.append(KFULL)
+
+vals = [float(i) for i in vals]
 
 
 def animation(i):
     plt.cla()
-    plt.plot(x_vals[:i])
-    if i < len(x_vals):
-        plt.xlabel('K: {}    P/L: {}    I: {}'.format(x_vals[i], x_vals[i] - K, i))
+    plt.plot(vals[:i])
+    if i < len(vals):
+        plt.xlabel('K: {}    P/L: {}    I: {}'.format(vals[i], vals[i] - K, i))
 
 
-ani = FuncAnimation(plt.gcf(), animation, interval=50)
-
+#ani = FuncAnimation(plt.gcf(), animation, interval=10)
+plt.plot(vals)
 plt.show()
--- src/simulator.py
+++ src/simulator.py
@@ -4,23 +4,11 @@ import random
 import colorama
 
 class Simulator(object):
-    
-    @staticmethod
-    def random_sample(filepaths: list, samples_length: int) -> list:
-        sample = random.choice(filepaths)
-        with open(sample, 'r') as f:
-            d = f.read().split()
-            d = [int(i) for i in d]
-            if len(d) < samples_length:
-                print(colorama.Fore.RED + 'File too short')
-            else:
-                r = random.choice(range(len(d) - samples_length + 1))
-                print(colorama.Fore.YELLOW + '{} {}:{}'.format(sample, r + 1, r + samples_length))
-                return d[r : r + samples_length]
 
 
     @staticmethod
     def simulate_bubbles_and_progression(numbers: list, config: dict, K: int) -> list:
+        K_begin = K
         result = list()
         bets = list()
         for i in range(len(numbers)):
@@ -28,10 +16,13 @@ class Simulator(object):
 
             for g, b in bets:
                 K -= b
-                if n[-1] in g:
-                    K += b * (len(config['numbers_without_greens']) / len(g))
-                elif n[-1] in config['greens']:
+
+                if n[-1] in config['greens']:
                     K += b / 2
+
+                elif n[-1] in g:
+                    K += b * (len(config['numbers_without_greens']) / len(g))
+
             result.append(K)
 
             bets.clear()
@@ -57,10 +48,12 @@ class Simulator(object):
                 bets.append((config['reds'], 0.02 * K))
 
 
+
             if len(lowers) == v1:
                 bets.append((config['highers'], 0.01 * K))
             elif len(lowers) == v1 + 1:
                 bets.append((config['highers'], 0.02 * K))
+
 
 
             if len(highers) == v1:
@@ -69,10 +62,12 @@ class Simulator(object):
                 bets.append((config['lowers'], 0.02 * K))
 
 
+
             if len(evens) == v1:
                 bets.append((config['odds'], 0.01 * K))
             elif len(evens) == v1 + 1:
                 bets.append((config['odds'], 0.02 * K))
+
 
             
             if len(odds) == v1:
@@ -80,6 +75,8 @@ class Simulator(object):
             elif len(odds) == v1 + 1:
                 bets.append((config['evens'], 0.02 * K))
 
+            if K >= 2 * K_begin or K < K_begin / 10:
+                break
             
         return result
             
