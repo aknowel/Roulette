9fcc9ba7182b8054d1f221451d189eae5b525efe
Paweł Nowak <paweln.nowak@student.uj.edu.pl>
2021-03-20 10:26:32 +0100

    One month in handwritten

--- src/generator.py
+++ src/generator.py
@@ -2,7 +2,7 @@ import random
 
 l = [i for i in range(37)]
 
-for i in range(1, 39):
+for i in range(1, 59):
     f = open('../data/generated/gen{}.txt'.format(i), 'w+')
     for j in range(500):
         f.write('{}\n'.format(random.choice(l)))
